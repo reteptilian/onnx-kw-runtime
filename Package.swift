2684794f9457540f7ea94fdaee7a6cc3e92cd33e
Peter Esbensen <pkesbensen@gmail.com>
2026-03-31 12:01:59 -0700

    fix targets

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "OnnxKWRuntime",
-            targets: ["OnnxKWRuntime"]
+            targets: ["OnnxRuntime", "SherpaOnnx"]
         )
     ],
     targets: [
